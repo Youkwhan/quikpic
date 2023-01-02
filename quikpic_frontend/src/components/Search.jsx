d597379f59e8d6df0f97592bbdd21cd9a7fb75ec
Youkwhan <Youkwhan@gmail.com>
2023-01-01 21:59:12 -0500

    search and feedQuery needs to be a function

--- quikpic_frontend/src/components/Search.jsx
+++ quikpic_frontend/src/components/Search.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
 
-export default Search
+const Search = ({ searchTerm, setSearchTerm }) => {
+	const [pins, setPins] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		if (searchTerm) {
+			setLoading(true);
+
+			const query = searchQuery(searchTerm.toLowerCase());
+
+			client.fetch(query).then((data) => {
+				setPins(data);
+				setLoading(false);
+			});
+		} else {
+			setLoading(true);
+
+			client.fetch(feedQuery()).then((data) => {
+				setPins(data);
+				setLoading(false);
+			});
+		}
+	}, [searchTerm]);
+
+	return (
+		<div>
+			{loading ? <Spinner message="Searching for pins..." /> : null}
+
+			{pins?.length !== 0 ? <MasonryLayout pins={pins} /> : null}
+
+			{pins?.length === 0 && searchTerm !== "" && !loading ? (
+				<div className="mt-10 text-center text-xl">No Pins Found!</div>
+			) : null}
+		</div>
+	);
+};
+
+export default Search;
--- quikpic_frontend/src/utils/data.js
+++ quikpic_frontend/src/utils/data.js
@@ -10,7 +10,7 @@ export const userQuery = (userId) => {
 
 export const searchQuery = (searchTerm) => {
 	// [in each pin it will search for matching title, category, about] {get_back image (which contains an asset ref and we only need URL), id, destination, postedBy, save[all the people who saved this pin] }
-	const query = `*[_type == "pin" && title match "${searchTerm}*" {}|| category match "${searchTerm}*" {}|| about match "${searchTerm}*"]{
+	const query = `*[_type == "pin" && title match "${searchTerm}*" || category match "${searchTerm}*" || about match "${searchTerm}*"]{
       image{
          asset -> {
             url
@@ -36,28 +36,31 @@ export const searchQuery = (searchTerm) => {
 };
 
 // get all pins,| in decending creation order, {specify what data we need}
-export const feedQuery = `*[_type == "pin"] | order(_createAt desc) {
-   image{
-      asset -> {
-         url
-      }
-   },
-   _id,
-   destination,
-   postedBy -> {
-      _id,
-      userName,
-      image
-   },
-   save[] {
-      _key,
-      postedBy -> {
-         _id,
-         userName,
-         image
-      },
-   },
-}`;
+export const feedQuery = () => {
+	const query = `*[_type == "pin"] | order(_createAt desc) {
+    image{
+        asset -> {
+          url
+        }
+    },
+    _id,
+    destination,
+    postedBy -> {
+        _id,
+        userName,
+        image
+    },
+    save[] {
+        _key,
+        postedBy -> {
+          _id,
+          userName,
+          image
+        },
+    },
+  }`;
+	return query;
+};
 
 export const categories = [
 	{
